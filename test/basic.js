77cd678bd2de39cbaf073813259f66b991a43ad4
isaacs <i@izs.me>
2012-11-16 19:53:53 +1100

    test

---
+++ test/basic.js
@@ -0,0 +1,76 @@
+var http = require('http');
+var test = require('tap').test;
+var server;
+var dhs = require('../');
+var fs = require('fs');
+var request = require('request');
+
+function req (u, t) {
+  request('http://localhost:1337/' + u, function(er, res, body) {
+    console.error('back', er, res.statusCode, res.headers, body);
+    if (er)
+      throw er;
+
+    t.equal(res.statusCode, 500, 'status code should be 500');
+    t.notOk(process.domain, 'domain should have exited');
+
+    // the error should reference this file first.
+    t.like(body, new RegExp('^' +
+      'Error: /' + u + '\n' +
+      '    at .*' + __filename + ':[0-9]+:[0-9]+\\)\n'));
+    t.end();
+  });
+}
+
+test('setup', function(t) {
+  console.error('setup');
+  server = http.createServer(function (req, res) {
+    dhs(req, res);
+    res.error = function(er) {
+      res.statusCode = 500;
+      res.end(er.stack || er.message);
+    };
+
+    function t() {
+      throw new Error(req.url);
+    }
+
+    switch (req.url) {
+      case '/st':
+        return setTimeout(t, 100);
+      case '/nt':
+        return process.nextTick(t);
+      case '/f':
+        return fs.readFile(__filename, t);
+      default:
+        t();
+    }
+  });
+  server.listen(1337, function() {
+    t.pass('listening');
+    t.end();
+  });
+});
+
+test('basic', function(t) {
+  req('', t);
+});
+
+test('setTimeout', function(t) {
+  req('st', t);
+});
+
+test('nextTick', function(t) {
+  req('nt', t);
+});
+
+test('readFile', function(t) {
+  req('f', t);
+});
+
+test('shutdown', function(t) {
+  server.close(function() {
+    t.pass('ok');
+    t.end();
+  });
+});
