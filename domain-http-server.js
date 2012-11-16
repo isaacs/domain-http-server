cdec81f3580a95c8122ac0fd04ca936796048612
isaacs <i@izs.me>
2012-11-16 19:53:49 +1100

    the code

---
+++ domain-http-server.js
@@ -0,0 +1,45 @@
+var domain = require('domain');
+module.exports = dhs;
+
+function dhs (req, res, opt, next) {
+  if (typeof opt === 'function')
+    next = opt, opt = {};
+
+  if (!opt)
+    opt = {};
+
+  var d = domain.create();
+  d.add(req);
+  d.add(res);
+
+  d.on('error', function (e) {
+    if (opt.exit !== false)
+      d.exit();
+
+    if (opt.close)
+      res.server.close();
+
+    if (typeof res.error === 'function')
+      res.error(e);
+
+    if (opt.callback)
+      opt.callback(e);
+
+    if (!res.error && !opt.callback)
+      throw e;
+  });
+
+  if (next)
+    next();
+
+  if (opt.enter !== false)
+    d.enter();
+
+  return d;
+}
+
+dhs.defaults = function (opt) {
+  return function (req, res, next) {
+    return dhs(req, res, opt, next)
+  };
+};
